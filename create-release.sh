f5d8357f4e52655566b7e2df2376ce50213c66ce
Luis Esperança <luis.esperanca@vwds.pt>
2022-12-15 12:04:28 +0000

    sleep

--- create-release.sh
+++ create-release.sh
@@ -5,8 +5,9 @@ LASTEST_TAG=$(git describe --tags --abbrev=0)
 echo "tag: " + $LASTEST_TAG
 gh release create "$LASTEST_TAG" --generate-notes --draft
 
+sleep 2
 # python parse parse
-notes=$(gh release view "$LASTEST_TAG" --json body --jq .body)
+notes=$(gh release view $LASTEST_TAG --json body --jq .body)
 notes_edit=$(python parse_release.py "$notes")
 echo "$notes_edit"
 #
