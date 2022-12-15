4a4c4c8e909ba1522929431114084eefbee98940
Luis Esperança <luis.esperanca@vwds.pt>
2022-12-15 11:44:25 +0000

    Comment stuff

--- create-release.sh
+++ create-release.sh
@@ -6,9 +6,9 @@ echo "tag: " + $LASTEST_TAG
 gh release create "$LASTEST_TAG" --generate-notes --draft
 
 # python parse parse
-notes=$(gh release view "$LASTEST_TAG" --json body --jq .body)
-notes_edit=$(python parse_release.py "$notes")
-echo "$notes_edit"
-
-# push new release
-gh release edit "$LASTEST_TAG" --notes "$notes_edit" --draft
+#notes=$(gh release view "$LASTEST_TAG" --json body --jq .body)
+#notes_edit=$(python parse_release.py "$notes")
+#echo "$notes_edit"
+#
+## push new release
+#gh release edit "$LASTEST_TAG" --notes "$notes_edit" --draft
