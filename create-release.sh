0f9a401b1448f3abcd6df731eaae9cf80e920da9
Luis Esperança <luis.esperanca@vwds.pt>
2022-12-15 11:04:39 +0000

    Fix release script

--- create-release.sh
+++ create-release.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 #create draft
-#LASTEST_TAG=$(git describe --tags --abbrev=0 | cut -c2-)
-LASTEST_TAG=v3
-#gh release create $LASTEST_TAG  --generate-notes --draft
+LASTEST_TAG=$(git describe --tags --abbrev=0 | cut -c2-)
+gh release create $LASTEST_TAG  --generate-notes --draft
 
 # python parse parse
 notes=$(gh release view $LASTEST_TAG --json body --jq .body)
